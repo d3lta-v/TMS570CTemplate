79ff7693970e2b275e6374f77dd0deb69dba2c3f
Pan Ziyue <random.rrr3@gmail.com>
2025-11-17 00:00:44 +0800

    feat:edited existing toolchain commands for compatibility with TI armcl compiler

--- project/cmake/ti_linux-toolchain.cmake
+++ project/cmake/ti_linux-toolchain.cmake
@@ -7,8 +7,11 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
+# WARNING: The following path is specific to the installation location of the TI compiler.
+# Please adjust it according to your system setup.
+
 set(TOOLCHAIN ti-cgt-arm)
-set(TOOLCHAIN_PREFIX "/opt/josepablocb/ti")
+set(TOOLCHAIN_PREFIX "/Applications/ti/ccs1281/ccs/tools/compiler/ti-cgt-arm_20.2.7.LTS")
 set(TOOLCHAIN_BIN_DIR ${TOOLCHAIN_PREFIX}/bin)
 set(TOOLCHAIN_INC_DIR ${TOOLCHAIN_PREFIX}/include)
 set(TOOLCHAIN_LIB_DIR ${TOOLCHAIN_PREFIX}/lib)
--- project/cmake/ti_toolchain.cmake
+++ project/cmake/ti_toolchain.cmake
@@ -57,17 +57,17 @@ set(CMAKE_EXE_LINKER_FLAGS "--heap_size=0x800 --stack_size=0x800 --rom_model --b
 #---------------------------------------------------------------------------------------
 
 # Options for DEBUG build
-# -Og   Enables optimizations that do not interfere with debugging.
+# -Ooff   Enables optimizations that do not interfere with debugging.
 # -g    Produce debugging information in the operating system’s native format.
-set(CMAKE_C_FLAGS_DEBUG "-Og -g -g3 -gdwarf-3 -gstrict-dwarf" CACHE INTERNAL "C Compiler options for debug build type")
-set(CMAKE_CXX_FLAGS_DEBUG "-Og -g -g3 -gdwarf-3 -gstrict-dwarf" CACHE INTERNAL "C++ Compiler options for debug build type")
-set(CMAKE_ASM_FLAGS_DEBUG "-g -g3 -gdwarf-3 -gstrict-dwarf" CACHE INTERNAL "ASM Compiler options for debug build type")
+set(CMAKE_C_FLAGS_DEBUG "-Ooff -g" CACHE INTERNAL "C Compiler options for debug build type")
+set(CMAKE_CXX_FLAGS_DEBUG "-Ooff -g" CACHE INTERNAL "C++ Compiler options for debug build type")
+set(CMAKE_ASM_FLAGS_DEBUG "-g" CACHE INTERNAL "ASM Compiler options for debug build type")
 set(CMAKE_EXE_LINKER_FLAGS_DEBUG "" CACHE INTERNAL "Linker options for debug build type")
 
 # Options for RELEASE build
-# -Os   Optimize for size. -Os enables all -O2 optimizations.
+# -O3   Optimize for size. -Os enables all -O2 optimizations.
 # -flto Runs the standard link-time optimizer.
-set(CMAKE_C_FLAGS_RELEASE "-Os -flto" CACHE INTERNAL "C Compiler options for release build type")
-set(CMAKE_CXX_FLAGS_RELEASE "-Os -flto" CACHE INTERNAL "C++ Compiler options for release build type")
+set(CMAKE_C_FLAGS_RELEASE "-O3 -flto" CACHE INTERNAL "C Compiler options for release build type")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -flto" CACHE INTERNAL "C++ Compiler options for release build type")
 set(CMAKE_ASM_FLAGS_RELEASE "" CACHE INTERNAL "ASM Compiler options for release build type")
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-flto" CACHE INTERNAL "Linker options for release build type")
